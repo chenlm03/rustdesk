514471290b045caf81b354878192e71b215960e5
chenlm03 <37399819+chenlm03@users.noreply.github.com>
2024-12-09 09:20:02 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,11 +100,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["101.132.132.209"];
+pub const PUBLIC_RS_PUB_KEY: &str = "H3XILnwzhXG3xKM00gi3ZXRLoz1mV+8+JsznAewvigA=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
-    Some(key) if !key.is_empty() => key,
+    Some（key） 如果 ！key.is_empty（） => 键，
     _ => PUBLIC_RS_PUB_KEY,
 };
 
